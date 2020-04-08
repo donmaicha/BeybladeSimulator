25445e746b6213184367c8559a2dffcadec9ce9f
donmaicha <genmaicha77783@gmail.com>
2020-04-08 21:47:11 +0900

    Initial Commit

---
+++ BeybladeSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeybladeSimulator
+//
+//  Created by 福島拓希 on 2020/04/08.
+//  Copyright © 2020 donmaicha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
